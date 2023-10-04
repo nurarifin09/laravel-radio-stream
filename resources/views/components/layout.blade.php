67622bf6eb4b2ebc8b5f25784a3d4b1fb546739f
Nur Arifin <16188544+nurarifin09@users.noreply.github.com>
2023-10-04 16:34:50 +0700

    Update layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,9 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Radio Islam Player</title>
+        <meta name="theme-color" content="#6777ef"/>
+        <link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
+        <link rel="manifest" href="{{ asset('/manifest.json') }}">
         <script src="https://cdn.tailwindcss.com?plugins=typography"></script>
     </head>
     <body class="min-h-screen bg-gray-50 font-sans text-black antialiased">
@@ -31,5 +34,22 @@
         @persist('player')
             <x-episode-player />
         @endpersist
+        <script src="{{ asset('/sw.js') }}"></script>
+        <script>
+           if ("serviceWorker" in navigator) {
+              // Register a service worker hosted at the root of the
+              // site using the default scope.
+              navigator.serviceWorker.register("/sw.js").then(
+              (registration) => {
+                 console.log("Service worker registration succeeded:", registration);
+              },
+              (error) => {
+                 console.error(`Service worker registration failed: ${error}`);
+              },
+            );
+          } else {
+             console.error("Service workers are not supported.");
+          }
+        </script>
     </body>
 </html>
